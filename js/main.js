64ca4014a3d57f3dac0c05490b44f08ef2c2d57f
Mohamed-Abdelfattah <moabdelfattah221@gmail.com>
2022-08-14 02:56:06 +0200

    push files

---
+++ js/main.js
@@ -0,0 +1,19 @@
+let textArea = document.querySelector(".textarea");
+let lowBtn = document.querySelector(".lower")
+let uppBtn = document.querySelector(".upper")
+let capBtn = document.querySelector(".capitalize")
+let clrBtn = document.querySelector(".clear")
+
+// Functions
+lowBtn.onclick = function(){
+    textArea.style.cssText = "text-transform: lowercase;";
+}
+uppBtn.onclick = function(){
+    textArea.style.cssText = "text-transform: uppercase;";
+}
+capBtn.onclick = function(){
+    textArea.style.cssText = "text-transform: capitalize;";
+}
+clrBtn.onclick = function(){
+    textArea.textContent = "";
+}
